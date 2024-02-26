944bcbf5078021035461ac8c614b998fae511794
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-02-26 23:30:33 +0330

    J86 react-context-api (create-admin-context-and-require-templates-js-files)

--- src/layouts/admin/Index.jsx
+++ src/layouts/admin/Index.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "./navbar/Navbar";
 import Sidebar from "./sidebar/Sidebar";
+import AdminContextContainer, {
+  AdminContext,
+} from "../../context/adminLayoutContext";
 
 const Index = () => {
+  const { showSidebar } = useContext(AdminContext);
   return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <section id="content_section" className="bg-light py-2 px-3"></section>
-    </div>
+    <AdminContextContainer>
+      <div>
+        <Navbar />
+        <Sidebar />
+        <section
+          id="content_section"
+          className={`bg-light py-2 px-3 ${
+            showSidebar ? "with_sidebar" : null
+          }`}
+        ></section>
+      </div>
+    </AdminContextContainer>
   );
 };
 
--- src/layouts/admin/navbar/RightContent.jsx
+++ src/layouts/admin/navbar/RightContent.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AdminContext } from "../../../context/adminLayoutContext";
 
 const RightContent = () => {
+  const { setShowSidebar } = useContext(AdminContext);
   return (
     <div className="right_content h-100 py-1 bg-dark">
       <a className="navbar-brand h-100" href="/">
@@ -11,6 +13,9 @@ const RightContent = () => {
           id="handle_toggle_sidemenu flexSwitchCheckChecked"
           className="form-check-input pointer"
           type="checkbox"
+          onChange={(e) => {
+            setShowSidebar(e.target.checked);
+          }}
         />
       </div>
     </div>
