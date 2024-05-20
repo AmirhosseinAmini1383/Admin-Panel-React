c1d5a292f6b65d577f3afdaff73c98df2a2d64f0
AmirhosseinAmini1383 <amirhossein.amini.ghahan83@gmail.com>
2024-05-20 21:28:58 +0330

    J114 add-category-attribute

--- src/pages/category/Attributes/AddAttributes.jsx
+++ src/pages/category/Attributes/AddAttributes.jsx
@@ -1,14 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik, Form, FastField } from "formik";
 
 import PaginatedTable from "../../../components/PaginatedTable";
-import AddCategory from "../AddCategory";
 import ShowInFilter from "./ShowInFilter";
 import AttrAction from "./AttrAction";
 import PrevPageButton from "../../../components/PrevPageButton";
-import { getCategoryAttributesService } from "../../../services/categoryAttributes";
-import { Alert } from "../../../utils/alerts";
+import {
+  addCategoryAttributesService,
+  getCategoryAttributesService,
+} from "../../../services/categoryAttributes";
 import FormikControl from "../../../components/form/FormikControl";
+import { Alert } from "../../../utils/alerts";
+
+const initialValues = {
+  title: "",
+  unit: "",
+  in_filter: true,
+};
+
+const onSubmit = async (values, actions, categoryId, setData) => {
+  try {
+    values = {
+      ...values,
+      in_filter: values.in_filter ? 1 : 0,
+    };
+    const res = await addCategoryAttributesService(categoryId, values);
+    if (res.status == 201) {
+      Alert("انجام شد", res.data.message, "success");
+      setData((oldData) => [...oldData, res.data.data]);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const validationSchema = Yup.object({
+  title: Yup.string()
+    .required("لطفا این قسمت را پر کنید")
+    .matches(
+      /^[\u0600-\u06FF\sa-zA-Z0-9@!%$?&]+$/,
+      "فقط از حروف و اعداد استفاده شود"
+    ),
+  unit: Yup.string()
+    .required("لطفا این قسمت را پر کنید")
+    .matches(
+      /^[\u0600-\u06FF\sa-zA-Z0-9@!%$?&]+$/,
+      "فقط از حروف و اعداد استفاده شود"
+    ),
+  in_filter: Yup.boolean(),
+});
 
 const AddAttributes = () => {
   const location = useLocation();
@@ -40,7 +82,6 @@ const AddAttributes = () => {
       const res = await getCategoryAttributesService(
         location.state.CategoryData.id
       );
-      console.log(res);
       if (res.status == 200) {
         setData(res.data.data);
       }
@@ -64,45 +105,60 @@ const AddAttributes = () => {
       </h6>
       <div className="container">
         <div className="row justify-content-center">
-          <div className="row my-3">
-            <div className="col-12 col-md-6 col-lg-4 my-1">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="عنوان ویژگی جدید"
-              />
-            </div>
-            <div className="col-12 col-md-6 col-lg-4 my-1">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="واحد ویژگی جدید"
-              />
-            </div>
-            <div className="col-8 col-lg-2 my-1">
-              <div className="form-check form-switch d-flex justify-content-center align-items-center p-0 h-100">
-                <label
-                  className="form-check-label pointer"
-                  htmlFor="flexSwitchCheckDefault"
-                >
-                  نمایش در فیلتر
-                </label>
-                <input
-                  className="form-check-input pointer mx-3"
-                  type="checkbox"
-                  id="flexSwitchCheckDefault"
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values, actions) =>
+              onSubmit(values, actions, location.state.CategoryData.id, setData)
+            }
+            validationSchema={validationSchema}
+          >
+            <Form>
+              <div className="row my-3">
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="title"
+                  label="عنوان"
+                  className="col-md-6 col-lg-4 my-1"
+                  placeholder="عنوان ویژگی جدید"
                 />
+
+                <FormikControl
+                  control="input"
+                  type="text"
+                  name="unit"
+                  label="واحد"
+                  className="col-md-6 col-lg-4 my-1"
+                  placeholder="واحد ویژگی جدید"
+                />
+                <div className="col-8 col-lg-2 my-1">
+                  <FormikControl
+                    control="switch"
+                    name="in_filter"
+                    label="نمایش در فیلتر"
+                  />
+                </div>
+                <div className="col-4 col-lg-2 d-flex justify-content-center align-items-start my-1">
+                  <FastField>
+                    {({ form }) => {
+                      return (
+                        <i
+                          className="fas fa-check text-light bg-success rounded-circle p-2 mx-1 hoverable_text hoverable pointer has_tooltip hoverable_text"
+                          title="ثبت ویژگی"
+                          data-bs-toggle="tooltip"
+                          data-bs-placement="top"
+                          onClick={() => {
+                            form.submitForm();
+                          }}
+                        ></i>
+                      );
+                    }}
+                  </FastField>
+                </div>
               </div>
-            </div>
-            <div className="col-4 col-lg-2 d-flex justify-content-center align-items-center my-1">
-              <i
-                className="fas fa-check text-light bg-success rounded-circle p-2 mx-1 hoverable_text hoverable pointer has_tooltip hoverable_text"
-                title="ثبت ویژگی"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-              ></i>
-            </div>
-          </div>
+            </Form>
+          </Formik>
+
           <hr />
 
           <PaginatedTable
--- src/services/categoryAttributes.js
+++ src/services/categoryAttributes.js
@@ -3,3 +3,11 @@ import httpService from "./httpservice";
 export const getCategoryAttributesService = (categoryId) => {
   return httpService(`/admin/categories/${categoryId}/attributes`, "get");
 };
+
+export const addCategoryAttributesService = (categoryId, data) => {
+  return httpService(
+    `/admin/categories/${categoryId}/attributes`,
+    "post",
+    data
+  );
+};
